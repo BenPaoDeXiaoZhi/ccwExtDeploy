36ce7a0114aa7e9e758390efc39caf4bc214a3d3
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-03 22:10:50 +0800

    更新 index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import fs from "fs"
 try {
   const pid = process.env.PROJECT_ID || "pid not set"
+  const banner = process.env.BANNER || ""
+  const src=process.env.src
+  let dat=fs.readFileSync(src)
   console.log(`using project ${pid}`)
+  console.log(`src: ${src}`)
+  console.log(`inject banner:${banner}`)
 } catch (error) {
   throw error
 }
