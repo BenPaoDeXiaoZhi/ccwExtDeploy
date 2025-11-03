1ac449d1f9e3223283ae8504947199a205005e47
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-03 21:48:56 +0800

    更新 index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
 
 try {
-  const pid = core.getInput("projectId");
+  const pid = process.env.projectId
   core.info(`using project ${pid}`)
 } catch (error) {
-  core.setFailed(error.message);
+  throw error
 }
