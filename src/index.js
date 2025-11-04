2881b14c49785b409c8cd41464dc89e194173408
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-04 17:06:44 +0800

    更新 index.js

--- src/index.js
+++ src/index.js
@@ -5,26 +5,29 @@ try {
   const banner = process.env.BANNER || ""
   const src=process.env.SRC
   const dst=process.env.DST
+  const token=process.env.TOKEN
+  const uid=token.slice(16)
   console.log(`using file: ${dst} in project ${pid}`)
   console.log(`src: ${src}`)
   console.log(`inject banner:${banner}`)
+  console.log(`uid: ${uid}`)
   let dat=fs.readFileSync(src)
-  start()
+  start(pid,dat,dst,token,uid)
 } catch (error) {
   throw error
 }
 
 function getExit(browser,ctx,page){
-  return async function(){
+  return async function(code=0,msg="exited successfully!"){
     await page.close();
     await ctx.close();
     await browser.close();
-    console.log("exited successfully!");
-    process.exit(0);
+    console.log(msg);
+    process.exit(code);
   }
 }
 
-async function start(pid,dat,dst){
+async function start(pid,dat,dst,token,uid){
   const browser = await chromium.launch()
   const ctx = await browser.newContext()
   const page = await ctx.newPage()
