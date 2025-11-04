c92af7192fda38e3fef22c096caecec21f04c685
BenPaoDeXiaoZhi <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2025-11-04 16:59:19 +0800

    Integrate Playwright and enhance logging
    Added Playwright integration for browser automation and improved logging.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import fs from "fs"
+import { chromium } from "playwright"
 try {
   const pid = process.env.PROJECT_ID || "pid not set"
   const banner = process.env.BANNER || ""
   const src=process.env.SRC
-  let dat=fs.readFileSync(src)
-  console.log(`using project ${pid}`)
+  const dst=process.env.DST
+  console.log(`using file: ${dst} in project ${pid}`)
   console.log(`src: ${src}`)
   console.log(`inject banner:${banner}`)
+  let dat=fs.readFileSync(src)
+  start()
 } catch (error) {
   throw error
+}
+
+function getExit(browser,ctx,page){
+  return async function(){
+    await page.close();
+    await ctx.close();
+    await browser.close();
+    console.log("exited successfully!");
+    process.exit(0);
+  }
+}
+
+async function start(pid,dat,dst){
+  const browser = await chromium.launch()
+  const ctx = await browser.newContext()
+  const page = await ctx.newPage()
+  const exit = getExit(browser,ctx,page)
+  await exit()
 }
